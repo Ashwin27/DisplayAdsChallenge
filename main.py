8fabbbe19e25fd4a0cd4f780b97bdccb8c4575d6
Ashwin Raman <a24venka@uwaterloo.ca>
2016-01-28 21:37:47 +0800

    Data Extraction from file using pandas complete

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import os
+import seaborn
 
 dir = os.path.dirname(__file__)
 
-data = pd.read_csv(dir + 'Data/dac_sample.txt', sep='\t')
+data = pd.read_csv(dir + 'Data/dac_sample.txt', sep='\t', header=None)
 print data.head()
 
+X = data.ix[:, 1:]
+Y = data.ix[:, 0]
