830886dd81853c93585fe75ce1daadd93280493d
Ashwin Raman <a24venka@uwaterloo.ca>
2016-01-29 13:40:46 +0800

    Working model with 77% accuracy

--- main.py
+++ main.py
@@ -1,11 +1,65 @@
-import pandas as pd
+## Kaggle Display Advertising Challenge
+##
+## Author: Ashwin Raman
+##
+## Last Edited: 28th Jan 2016
+########################################
+
 import os
-import seaborn
+import numpy as ny
+
+# Data Extraction from file into data frame
+import pandas as pd
+
+# Model - K Nearest Neighbors
+from sklearn.neighbors import KNeighborsClassifier
+
+# Preprocessing data 
+# 1. Removing nulls (NaNs)
+# 2. Encoding categorical variables with numerals
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import LabelEncoder
+
+# Crossvalidation - Spliting training data into training 
+# 					and test and measuring performance
+from sklearn.cross_validation import train_test_split
+from sklearn import metrics 
 
 dir = os.path.dirname(__file__)
 
-data = pd.read_csv(dir + 'Data/dac_sample.txt', sep='\t', header=None)
-print data.head()
+data = pd.read_csv(dir + '/home/ashwin/Academics/Machine-Learning/Display-Ads/Data/dac_sample.txt', sep='\t', header=None)
+#print data.head()
 
-X = data.ix[:, 1:]
-Y = data.ix[:, 0]
+enc = LabelEncoder()
+
+categorical_data = data.ix[:, 14:]
+numerical_data = data.ix[:, 1:13]
+
+#print categorical_data.head()
+#print numerical_data.head()
+#print categorical_data.ix[:, 14]
+
+#populate feature array processed
+X = numerical_data
+for i in range(14, 40):
+	test = enc.fit_transform(categorical_data.ix[:, i])
+	test = pd.DataFrame(test, columns=[i])
+	X = X.join(test)
+	
+#print X.head()
+
+imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+
+X = pd.DataFrame(imp.fit_transform(X), columns=range(1, 40))
+y = data.ix[:, 0]
+
+#print X.head()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+
+knn = KNeighborsClassifier(n_neighbors=50)
+knn.fit(X_train, y_train)
+
+y_pred = knn.predict(X_test)
+
+print metrics.accuracy_score(y_test, y_pred)
